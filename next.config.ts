7fb0df5fdebd6848c98ae6b2ceba7aaf2b27e458
William Perez <wil.a.zz261102@gmail.com>
2026-02-05 21:17:16 -0500

    Probando

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/vive-tu-red-frontend",
-  images: {
-    unoptimized: true,
-  },
+const nextConfig = {
+  output: 'export',
+  distDir: 'docs',
+  basePath: '/vive-tu-red-frontend',
+  assetPrefix: '/vive-tu-red-frontend/',
 };
 
 export default nextConfig;
